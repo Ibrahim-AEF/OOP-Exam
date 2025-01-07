a4dbab9243fcda871c6e5ca313d80c1cf6ab04d8
200017733@must.edu.eg <200017733@must.edu.eg>
2025-01-07 10:06:09 +0200

    Exam

---
+++ OOP-Exam/Exam.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exam
+{
+    internal class Exam
+    {
+        public string ExamType { get; set; }
+        public List<Question> Questions { get; set; } = new List<Question>();
+        public int TotalMarks { get; set; } = 0;
+        public void StartExam()
+        {
+            int score = 0;
+            foreach (var questions in Questions)
+            {
+                Console.WriteLine($"Q: {questions.Body} ({questions.Mark} Marks");
+                for (int i = 0; i < questions.Choices.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1} : {questions.Choices[i]}");
+                }
+                int answer;
+                while (true)
+                {
+                    Console.Write("Your Answer (Enter a valid choice number): ");
+                    if (int.TryParse(Console.ReadLine(), out answer) && answer > 0 && answer <= questions.Choices.Count)
+                        break;
+
+                    Console.WriteLine("Invalid choice. Please enter a valid number corresponding to the choices.");
+                }
+                if (answer == questions.CorrectAnswer)
+                {
+                    Console.WriteLine("Correct Answer :)");
+                    score += questions.Mark;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Answer :(");
+                }
+            }
+            Console.WriteLine($"Your Score : {score}/{TotalMarks}");
+        }
+    }
+}
--- OOP-Exam/Program.cs
+++ OOP-Exam/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter Exam Type : [1 for Practical | 2 for Final] : ");
+            int examtype = int.Parse(Console.ReadLine());
+            Exam exam = new Exam();
+            exam.ExamType = examtype == 1 ? "Practical" : "Final";
+            Console.WriteLine("Enter Numbere Of Questions : ");
+            int NumOfQuestions = int.Parse(Console.ReadLine());
+            for (int i = 0; i < NumOfQuestions; i++)
+            {
+                Console.WriteLine($"Enter Question {i + 1} Details : ");
+                Question question = new Question();
+                Console.WriteLine("Enter Question Body : ");
+                question.Body = Console.ReadLine();
+                Console.WriteLine("Enter Question Marks : ");
+                question.Mark = int.Parse(Console.ReadLine());
+                question.Choices = new List<string>();
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Enter Choice {j + 1}: ");
+                    question.Choices.Add(Console.ReadLine());
+                }
+                Console.Write("Enter Correct Choice Number (1 - 2 - 3): ");
+                int CorrectChoice;
+                while (!int.TryParse(Console.ReadLine(), out CorrectChoice) || CorrectChoice < 1 || CorrectChoice > 3)
+                {
+                    Console.WriteLine("Invalid input. Please enter 1, 2, 3 for the correct choice.");
+                }
+                question.CorrectAnswer = CorrectChoice;
+                exam.Questions.Add(question);
+                exam.TotalMarks += question.Mark;
+            }
+            Console.WriteLine("Do You Want To Start The Exam ? (Yes/No) : ");
+            string word = Console.ReadLine();
+            if (word == "Yes" || word == "yes")
+            {
+                exam.StartExam();
+            }
+            else
+            {
+                Console.WriteLine("Thank You :)");
+            }
         }
     }
 }
